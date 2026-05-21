308ed471d6e3f7b339fa128aa54825a88df3152e
kicode91 <kinza2@icloud.com>
2026-05-21 17:32:14 +0100

    Create quotes.py

---
+++ quotes.py
@@ -0,0 +1,11 @@
+import random
+
+quotes = [
+    "Success is built daily.",
+    "Small progress still matters.",
+    "Consistency beats motivation.",
+    "Learning never stops.",
+    "Focus creates results."
+]
+
+print(random.choice(quotes))
